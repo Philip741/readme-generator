15d60e2d41ae3ea5e0a3c20c99365ecebc4cdc70
Philip Browning <philip741@gmail.com>
2021-10-05 10:50:57 -0400

    Added functionality to generate table of contents. Added section for license link to readme output Added License badge to top of readme file

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 var prompt = inquirer.createPromptModule();
+const licLinks = generateMarkdown.licenseLinks;
+const licenses = Object.keys(licLinks);
+
+const licenseChoices = [
+    "MIT",
+    "BSD",
+    "Apache",
+    "GPL"
+]
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -12,16 +21,11 @@ const questions = [
    { type: 'input',name: 'usage', message: "Please enter any usage information"},
    { type: 'input',name: 'contribution', message: "Please enter the contribution guidelines"},
    { type: 'input',name: 'testing', message: "Please enter any testing instructions"},
-   { type: 'input',name: 'git-username', message: "Please enter your github username"},
-   { type: 'input',name: 'email-address', message: "Please enter your email address"}
+   { type: 'input',name: 'git_username', message: "Please enter your github username"},
+   { type: 'input',name: 'email_address', message: "Please enter your email address"},
+   { type: 'list', name: 'license', message: "Choose a license type for the project", choices: licenses}
 ];
 
-prompt(questions)
-.then((answers) => {
-   console.log('running write to file')
-   const markdown = generateMarkdown(answers);
-   writeToFile('./test.md',markdown);
-})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -33,8 +37,17 @@ function writeToFile(fileName, data) {
 }
 
 
+
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   prompt(questions)
+   .then((answers) => {
+       //generateMarkdown.tableContents(answers);
+       console.log('running write to file')
+       const markdown = generateMarkdown.generateMarkdown(answers);
+       writeToFile('./test.md',markdown);
+   })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,54 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseBadges = {
+  Apache: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  BSD: "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+}
+const licenseLinks = {
+  Apache: "[License: Apache2](https://opensource.org/licenses/Apache-2.0)",
+  MIT: "[License: MIT](https://opensource.org/licenses/MIT)",
+  BSD: "[License: BSD3](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  // function pass in license as object list and return badge graphic and link
+  // If there is no license, return an empty string
+  if (license) {
+  return licenseBadges[license]
+  }
+  else { return "" }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  // function pass in license as object list and return link to license page
+  // if there is no license return empty string
+  if (license) {
+   return licenseLinks[license]
+  }
+  else { return "" } 
+}
+
+function tableContents (data) {
+    let tableVals = []
+    sections = Object.keys(data);
+    for (let i=0;i < sections.length; i++) {
+        tableVals.push(`[${sections[i]}](https://github.com/${data.git_username}/#${sections[i]})`);
+    }
+    //return tableVals.forEach((v) => v)
+    return tableVals
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // function to render markdown from object data
   console.log(data);
+  let tableOfContents = tableContents(data);
+  contentData = tableOfContents.join('\n');
   data =  
-  `# ${data.title}
+  `${renderLicenseBadge(data.license)}
+   # ${data.title}
    ## Table of contents     
+   ${contentData}
    ## Description
    ${data.description}   
    ## Instructions
@@ -26,9 +59,17 @@ function generateMarkdown(data) {
    ${data.contribution}
    ## Testing instructions
    ${data.testing}
-   
+   ## Questions
+   Email: ${data.email_address}
+   Github Username: ${data.git_username}
+   ## License
+   ${renderLicenseLink(data.license)}
   `;
   return data;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown,
+  licenseLinks,
+  tableContents,
+}
